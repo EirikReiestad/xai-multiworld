39d082cd7d99f67cb3ab88937f02489408a7ab82
Eirik Reiestad <eirik@reiestad.no>
2024-12-01 10:45:46 +0100

    feat: adding Position class

--- multigrid/utils/position.py
+++ multigrid/utils/position.py
@@ -1,4 +1,8 @@
 from dataclasses import dataclass
+from typing import Union
+
+import numpy as np
+from numpy.typing import NDArray
 
 
 @dataclass(frozen=True)
@@ -22,6 +26,15 @@ class Position:
         """Return the position as a tuple."""
         return (self.x, self.y)
 
+    def __ge__(self, other: "Position") -> bool:
+        """Greater than or equal to comparison."""
+        return self.x >= other.x and self.y >= other.y
+
+    def __lt__(self, other: tuple[int, int]) -> bool:
+        print(other, "TEST")
+        """Less than comparison."""
+        return self.x < other[0] and self.y < other[1]
+
     def distance_to(self, other: "Position") -> float:
         """Calculate Euclidean distance between two positions."""
         return ((self.x - other.x) ** 2 + (self.y - other.y) ** 2) ** 0.5
@@ -29,3 +42,13 @@ class Position:
     def to_tuple_reversed(self) -> tuple[int, int]:
         """Return the position as a tuple with the values reversed (y, x)."""
         return (self.y, self.x)
+
+    @classmethod
+    def from_list(cls, pos: list[tuple[int, int]]) -> NDArray["Position"]:
+        """Create a Position object from a list."""
+        x, y = zip(*pos)
+        return np.array([cls(*p) for p in zip(x, y)])
+
+    def to_list(self) -> list[tuple[int, int]]:
+        """Return the position as a list."""
+        return [(self.x, self.y)]
