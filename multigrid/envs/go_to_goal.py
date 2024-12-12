c6ab1f27b3e9355eb337d9364d4cd2a01876cb60
Eirik Reiestad <eirik@reiestad.no>
2024-12-13 00:50:31 +0100

    feat: dqn

--- multigrid/envs/go_to_goal.py
+++ multigrid/envs/go_to_goal.py
@@ -1,7 +1,10 @@
 from multigrid.base import MultiGridEnv
 from multigrid.core.grid import Grid
-from multigrid.core.world_object import Box, Wall, Goal
+from multigrid.core.world_object import Box, Goal, Wall
 from multigrid.utils.position import Position
+from multigrid.utils.typing import AgentID, ObsType
+from multigrid.core.action import Action
+from typing import Any, SupportsFloat
 
 
 class GoToGoalEnv(MultiGridEnv):
@@ -16,3 +19,19 @@ class GoToGoalEnv(MultiGridEnv):
             placeable_positions = self.grid.get_placeable_positions()
             pos = self._rand_elem(placeable_positions)
             agent.state.pos = pos
+
+    def step(
+        self, actions: dict[AgentID, Action | int]
+    ) -> tuple[
+        dict[AgentID, ObsType],
+        dict[AgentID, SupportsFloat],
+        dict[AgentID, bool],
+        dict[AgentID, bool],
+        dict[AgentID, dict[str, Any]],
+    ]:
+        observations, rewards, terminations, truncations, info = super().step(actions)
+        for agent in self.agents:
+            if self.grid.get(agent.state.pos) == Goal:
+                self.on_success(agent, rewards, terminations)
+
+        return observations, rewards, terminations, truncations, info
