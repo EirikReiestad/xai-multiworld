95948d68bbb8cbf56e93faef4c63bba948432c3e
Eirik Reiestad <eirik@reiestad.no>
2024-12-13 01:07:57 +0100

    feat: something

--- examples/single_agent_go_to_goal_static.py
+++ examples/single_agent_go_to_goal_static.py
@@ -2,7 +2,7 @@ from rllib.algorithms.dqn.dqn_config import DQNConfig
 from rllib.algorithms.dqn.dqn import DQN
 from multigrid.envs.go_to_goal import GoToGoalEnv
 
-env = GoToGoalEnv(width=11, height=11, max_steps=300, agents=3)
+env = GoToGoalEnv(width=7, height=7, max_steps=300, agents=10)
 
 config = (
     DQNConfig(batch_size=8)
@@ -10,7 +10,7 @@ config = (
     .training()
     .debugging(log_level="INFO")
     .rendering()
-    .wandb(project="multigrid-go-to-goal")
+    # .wandb(project="multigrid-go-to-goal")
 )
 
 dqn = DQN(config)
