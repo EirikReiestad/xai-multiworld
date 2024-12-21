6c117d22fab2934410f91800200560c349f1e0b9
Eirik Reiestad <eirik@reiestad.no>
2024-12-21 13:27:24 +0100

    feat: adding model loading

--- xailib/core/linear_probing/__main__.py
+++ xailib/core/linear_probing/__main__.py
@@ -1,8 +1,30 @@
-from typing import Mapping, Any
-from utils.core.model_loader import ModelLoader
 import os
+from typing import Any, Mapping
+
+from rllib.algorithms.dqn.dqn import DQN
+from rllib.algorithms.dqn.dqn_config import DQNConfig
+from utils.core.model_loader import ModelLoader
+from multigrid.envs.go_to_goal import GoToGoalEnv
 
 path = os.path.join("artifacts")
 
-model_loader = ModelLoader.load_from_path(path)
-print(model_loader)
+env = GoToGoalEnv(render_mode="rgb_array")
+
+config = (
+    DQNConfig(
+        batch_size=16,
+        replay_buffer_size=10000,
+        gamma=0.99,
+        learning_rate=1e-4,
+        eps_start=0.9,
+        eps_end=0.05,
+        eps_decay=50000,
+        target_update=1000,
+    )
+    .debugging(log_level="INFO")
+    .environment(env=env)
+)
+
+dqn = DQN(config)
+
+models = ModelLoader.load_from_path(path, dqn.model)
