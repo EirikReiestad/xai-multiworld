38845e2e9817648f5a5c9fb1c6dcfd34f0185b46
eirik <eirik@reiestad.no>
2024-12-28 23:31:53 +0100

    idun

--- examples/cleanup.py
+++ examples/cleanup.py
@@ -5,14 +5,23 @@ from multigrid.envs.cleanup import CleanUpEnv
 env = CleanUpEnv(
     width=7,
     height=7,
-    max_steps=200,
-    boxes=10,
+    max_steps=1000,
+    boxes=7,
     agents=6,
     success_termination_mode="any",
 )
 
 config = (
-    DQNConfig(batch_size=16)
+    DQNConfig(
+        batch_size=32,
+        replay_buffer_size=10000,
+        gamma=0.99,
+        learning_rate=1e-4,
+        eps_start=0.9,
+        eps_end=0.05,
+        eps_decay=2000000,
+        target_update=2000,
+    )
     .environment(env=env)
     .training()
     .debugging(log_level="INFO")
