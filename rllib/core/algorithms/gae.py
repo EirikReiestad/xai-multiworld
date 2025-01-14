feat: fixed the gradients!

rllib/core/algorithms/gae.py
@@ -2,8 +2,8 @@
 source: https://nn.labml.ai/rl/ppo/gae.html
 """
 
-import numpy as np
-from numpy.typing import NDArray
+import torch
+from typing import List
 
 
 class GAE:
@@ -13,17 +13,27 @@ class GAE:
         self.gamma = gamma
         self.lambda_ = lambda_
 
-    def __call__(self, done: NDArray, rewards: NDArray, values: NDArray) -> NDArray:
-        advantages = np.zeros((self.n_workers, self.worker_steps), dtype=np.float32)
-        last_advantage = 0
-        last_mask = 1.0 - done[:, -1]
-        last_value = values[:, -1] * last_mask
+    def __call__(
+        self,
+        done: List[List[bool]],
+        rewards: List[List[float]],
+        values: List[List[torch.Tensor]],
+    ) -> List:
+        advantages = []
+        last_advantage = [0 for _ in range(self.n_workers)]
+        last_mask = [1.0 - d[-1] for d in done]
+        last_value = [v[-1] * last_mask[i] for i, v in enumerate(values)]
 
         for t in reversed(range(self.worker_steps)):
-            mask = 1.0 - done[:, t]
-            delta = rewards[:, t] + self.gamma * last_value - values[:, t]
-            last_advantage = delta + self.gamma * self.lambda_ * last_advantage
-            advantages[:, t] = last_advantage
-            last_value = values[:, t] * mask
-
+            mask = [1.0 - d[t] for d in done]
+            delta = [
+                rewards[i][t] + self.gamma * last_value[i] - values[i][t]
+                for i in range(self.n_workers)
+            ]
+            last_advantage = [
+                delta[i] + self.gamma * self.lambda_ * last_advantage[i]
+                for i in range(self.n_workers)
+            ]
+            advantages.append(last_advantage)
+            last_value = [values[i][t] * mask[i] for i in range(self.n_workers)]
         return advantages

rllib/utils/ppo/calculations.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 from numpy.typing import NDArray
+from typing import List
 
 
 def ppo_loss(
@@ -13,7 +14,7 @@ def ppo_loss(
     surr1 = ratios * advantages
     surr2 = torch.clamp(ratios, 1 - epsilon, 1 + epsilon) * advantages
     policy_loss = -torch.min(surr1, surr2).mean()
-    return torch.tensor(float(policy_loss), requires_grad=True)
+    return policy_loss
     # NOTE: There are other improvements to the loss that I believe codebases like stable-baselines3 implement, which include value loss and entropy loss.
     # Might implement later. Suggestion:
     # value_loss = F.mse_loss(values, returns)
@@ -21,19 +22,9 @@ def ppo_loss(
     # return policy_loss + 0.5 * value_loss + 0.01 * entropy_loss
 
 
-def compute_log_probs(actions: NDArray, action_probs: NDArray) -> NDArray[np.float32]:
-    assert (
-        actions.shape[:2] == action_probs.shape[:2]
-    ), f"actions: {actions.shape}, action_probs: {action_probs.shape}"
-    log_probs = []
-    for i in range(actions.shape[1]):
-        log_probs.append(_compute_log_prob(actions[:, i], action_probs[:, i]))
-    return np.stack(log_probs, axis=1)
-
-
-def _compute_log_prob(actions: NDArray, action_probs: NDArray) -> NDArray[np.float32]:
-    log_probs = []
-    for action, action_prob in zip(actions, action_probs):
-        prob = action_prob[int(action)]
-        log_probs.append(torch.log(torch.tensor(prob, dtype=torch.float32)))
-    return np.array(log_probs, dtype=np.float32)
+def compute_log_probs(
+    actions: torch.Tensor, action_probs: torch.Tensor
+) -> torch.Tensor:
+    selected_action_probs = action_probs.gather(dim=-1, index=actions.unsqueeze(-1))
+    log_probs = torch.log(selected_action_probs)
+    return log_probs