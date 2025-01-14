feat: fixed the gradients!

rllib/utils/torch/processing.py
@@ -1,11 +1,22 @@
 from itertools import chain
 from numpy.typing import NDArray
-from typing import Dict, SupportsFloat, List
+from typing import Dict, SupportsFloat, List, Any
 from multigrid.utils.typing import ObsType
 
 import gymnasium as gym
 import numpy as np
 import torch
+
+
+def torch_stack_inner_list(value: List) -> torch.Tensor:
+    return torch.stack([torch.stack(v) for v in value])
+
+
+def torch_stack_inner_list_any(value: List) -> torch.Tensor:
+    result = []
+    for lst in value:
+        result.append(torch.stack([torch.tensor(v) for v in lst]))
+    return torch.stack(result)
 
 
 def observation_to_torch(

utils/common/numpy_collections.py
@@ -1,6 +1,7 @@
-from numpy.typing import NDArray
 import numpy as np
 import json
+import torch
+from numpy.typing import NDArray
 from typing import Any
 
 
@@ -30,10 +31,3 @@ def remove_nan(arr: NDArray) -> NDArray:
         if isinstance(a, np.ndarray) or not np.isnan(a):
             nan_arr.append(a)
     return np.array(nan_arr)
-
-
-def dict_list_to_ndarray(dict_list: list[dict[str, Any]]) -> NDArray:
-    data = []
-    for key in dict_list[0].keys():
-        data.append(np.array([d[key] for d in dict_list]))
-    return np.stack(data)