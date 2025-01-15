feat: trying to fix the lack of gradient in ppo algo

rllib/algorithms/ppo/ppo.py
@@ -23,6 +23,8 @@ from utils.common.collections import zip_dict_list
 PPO Paper: https://arxiv.org/abs/1707.06347
 """
 
+torch.autograd.set_detect_anomaly(True)
+
 
 class PPO(Algorithm):
     _policy_net: ActorCriticMultiInputNetwork
@@ -78,7 +80,7 @@ class PPO(Algorithm):
         )
 
     def predict(self, observation: Dict[AgentID, ObsType]) -> Dict[AgentID, int]:
-        actions, action_probs, values = self._predict(observation)
+        actions, action_probs, values = self._predict(observation, requires_grad=True)
         self._action_probs = action_probs
         self._values = values
         return actions
@@ -97,7 +99,7 @@ class PPO(Algorithm):
         action_probs = {}
         values = {}
         for key, action_prob in zip(observation.keys(), action_probabilities):
-            action_probs[key] = action_prob
+            action_probs[key] = action_prob.detach()
             actions[key] = self._get_action(action_prob)
             values[key] = policy_values[key]
         return actions, action_probs, values
@@ -172,7 +174,8 @@ class PPO(Algorithm):
             torch_stack_inner_list(action_batch_torch),
             torch_stack_inner_list(new_action_probs_batch),
         )
-        advantages_tensor = torch.tensor(advantages, requires_grad=True)
+
+        advantages_tensor = torch_stack_inner_list(advantages)
 
         loss = ppo_loss(
             log_probs,

rllib/core/algorithms/gae.py
@@ -23,8 +23,8 @@ class GAE:
         last_advantage: List[torch.Tensor] = [
             torch.tensor(0) for _ in range(self.n_workers)
         ]
-        last_mask = [1.0 - d[-1] for d in done]
-        last_value = [v[-1] * last_mask[i] for i, v in enumerate(values)]
+        mask = [1.0 - d[-1] for d in done]
+        last_value = [v[-1] * mask[i] for i, v in enumerate(values)]
 
         for t in reversed(range(self.worker_steps)):
             mask = [1.0 - d[t] for d in done]
@@ -38,4 +38,6 @@ class GAE:
             ]
             advantages.append(last_advantage)
             last_value = [values[i][t] * mask[i] for i in range(self.n_workers)]
-        return advantages
+        advantages_transposed = list(zip(*advantages))
+        assert len(values) == len(advantages_transposed)
+        return advantages_transposed