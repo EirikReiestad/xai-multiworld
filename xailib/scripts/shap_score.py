2458e1650f925d066d5adf0680ce84b1eebcf522
eirik <eirik@reiestad.no>
2025-01-22 14:56:32 +0100

    shap

--- xailib/scripts/shap_score.py
+++ xailib/scripts/shap_score.py
@@ -3,12 +3,14 @@ import os
 
 import numpy as np
 import shap
+from torch import norm
 
 from multigrid.envs.go_to_goal import GoToGoalEnv
 from multigrid.utils.wrappers import Observations
 from rllib.algorithms.dqn.dqn import DQN
 from rllib.algorithms.dqn.dqn_config import DQNConfig
 from rllib.utils.torch.processing import observations_seperate_to_torch
+from utils.common.observation import normalize_observations
 
 if __name__ == "__main__":
     path = os.path.join("assets", "observations", "observations.json")
@@ -45,10 +47,14 @@ if __name__ == "__main__":
 
     model = dqn.model
 
+    normalized_observations = normalize_observations(flattened_agent_observations)
     obs = observations_seperate_to_torch(flattened_agent_observations)
     explainer = shap.GradientExplainer(model, obs)
     shap_values = explainer.shap_values(obs)
 
     mean_shap_values = np.array(shap_values[0]).mean(axis=-1)
-    print(mean_shap_values.shape)
-    shap.image_plot(mean_shap_values, np.array(obs[0]))
+
+    pixel_values = np.array(obs[0])
+
+    for idx in range(mean_shap_values.shape[0]):
+        shap.image_plot(mean_shap_values[idx], pixel_values[idx])
