5516fcba6d4c322aa73e325083d3650d1e427d1d
eirik <eirik@reiestad.no>
2025-01-24 16:27:23 +0100

    feat:flock

--- examples/flock.py
+++ examples/flock.py
@@ -7,29 +7,29 @@ env = FlockEnv(
     height=1000,
     max_steps=1000,
     agents=100,
-    success_termination_mode="all",
-    render_mode="human",
     object_size=8,
-    agent_view_size=101,
+    agent_view_size=65,
+    success_termination_mode="all",
+    render_mode="rgb_array",
 )
 
 config = (
     DQNConfig(
-        batch_size=32,
-        replay_buffer_size=10000,
+        batch_size=128,
+        replay_buffer_size=100000,
         gamma=0.99,
         learning_rate=1e-4,
         eps_start=0.2,
         eps_end=0.05,
-        eps_decay=100000,
-        target_update=1000,
+        eps_decay=2000000,
+        target_update=5000,
     )
     .network(conv_layers=())
     .environment(env=env)
     .training()
     .debugging(log_level="INFO")
     .rendering()
-    # .wandb(project="multigrid-cleanupv0")
+    .wandb(project="mw-flock")
 )
 
 dqn = DQN(config)
