6e7119e2e9d3f3f345777223206f46dfa1cec740
eirik <eirik@reiestad.no>
2025-01-24 16:20:56 +0100

    feat: fixed a eps threshold err

--- examples/go_to_goal.py
+++ examples/go_to_goal.py
@@ -3,30 +3,31 @@ from rllib.algorithms.dqn.dqn import DQN
 from rllib.algorithms.dqn.dqn_config import DQNConfig
 
 env = GoToGoalEnv(
-    width=7,
-    height=7,
-    max_steps=4,
-    agents=3,
+    width=20,
+    height=20,
+    max_steps=300,
+    agents=20,
+    agent_view_size=9,
     success_termination_mode="all",
-    render_mode="human",
+    render_mode="rgb_array",
 )
 
 config = (
     DQNConfig(
-        batch_size=32,
+        batch_size=64,
         replay_buffer_size=10000,
         gamma=0.99,
         learning_rate=1e-4,
-        eps_start=0.2,
+        eps_start=0.9,
         eps_end=0.05,
-        eps_decay=1000000,
-        target_update=1000,
+        eps_decay=2000000,
+        target_update=5000,
     )
     .environment(env=env)
     .training()
     .debugging(log_level="INFO")
     .rendering()
-    # .wandb(project="multigrid-cleanupv0")
+    .wandb(project="test")
 )
 
 dqn = DQN(config)
