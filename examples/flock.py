b505a04a1f57cd9a0fbd073080a9faf801b7b38c
Eirik Reiestad <eirre@ntnu.no>
2025-01-31 16:49:05 +0100

    model

--- examples/flock.py
+++ examples/flock.py
@@ -4,10 +4,10 @@ from rllib.algorithms.dqn.dqn_config import DQNConfig
 from multiworld.swarm.envs.flock import FlockEnv
 
 env = FlockEnv(
-    width=500,
-    height=500,
+    width=200,
+    height=200,
     max_steps=1000,
-    agents=100,
+    agents=20,
     observations=10,
     predators=2,
     predator_steps=100,
@@ -25,10 +25,10 @@ config = (
         batch_size=64,
         replay_buffer_size=10000,
         gamma=0.99,
-        learning_rate=1e-4,
+        learning_rate=3e-4,
         eps_start=0.9,
         eps_end=0.05,
-        eps_decay=100000,
+        eps_decay=200000,
         target_update=2000,
     )
     .network(conv_layers=())
