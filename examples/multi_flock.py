f5ea7372c1761a2e219003b17208da957d22ce0b
eirik <eirik@reiestad.no>
2025-02-04 16:59:10 +0100

    feat: birds from a model:)

--- examples/multi_flock.py
+++ examples/multi_flock.py
@@ -1,13 +1,15 @@
-# from multiworld.swarm.utils.wrappers import SwarmConceptObsWrapper
-from rllib.algorithms.dqn.dqn import DQN
-from rllib.algorithms.dqn.dqn_config import DQNConfig
 from multiworld.swarm.envs.flock import FlockEnv
+from rllib.algorithms.algorithm_config import AlgorithmConfig
+from rllib.algorithms.dqn.dqn_config import DQNConfig
+from rllib.algorithms.dqn.mdqn import MDQN
+
+agents = 10
 
 env = FlockEnv(
     width=200,
     height=200,
     max_steps=400,
-    agents=20,
+    agents=agents,
     observations=10,
     predators=1,
     predator_steps=100,
@@ -17,29 +19,33 @@ env = FlockEnv(
     render_mode="human",
 )
 
-# env = ObservationCollectorWrapper(env, observations=10)
-# env = SwarmConceptObsWrapper(env, observations=10, method="random")
-
 config = (
     DQNConfig(
         batch_size=64,
         replay_buffer_size=100000,
         gamma=0.99,
-        learning_rate=1e-4,
-        eps_start=0.9,
+        learning_rate=3e-4,
+        eps_start=0.2,
         eps_end=0.05,
         eps_decay=100000,
-        target_update=5000,
+        target_update=1000,
     )
-    .network(conv_layers=())
     .environment(env=env)
-    .training(model="model_12800:v1")
+    .training()
     .debugging(log_level="INFO")
     .rendering()
-    .wandb(project="birds", log_interval=100)
 )
 
-dqn = DQN(config)
+mconfig = (
+    AlgorithmConfig("DQN")
+    .environment(env=env)
+    .training()
+    .debugging(log_level="INFO")
+    .rendering()
+    .wandb(project="birds", log_interval=10 * agents)
+)
+
+dqn = MDQN(agents, mconfig, config)
 
 while True:
     dqn.learn()
