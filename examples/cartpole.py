513a854d673b0a2b0d3c2e7062944404c9c77625
Eirik Reiestad <47319221+EirikReiestad@users.noreply.github.com>
2025-02-07 09:53:32 +0100

    feat: rl scheduler (#22)

--- examples/cartpole.py
+++ examples/cartpole.py
@@ -1,4 +1,5 @@
 import gymnasium as gym
+from torch.optim import lr_scheduler
 
 from rllib.algorithms.dqn.dqn import DQN
 from rllib.algorithms.dqn.dqn_config import DQNConfig
@@ -12,20 +13,21 @@ env = GymnasiumWrapper(env)
 
 config = (
     DQNConfig(
-        batch_size=64,
+        batch_size=128,
         replay_buffer_size=10000,
         gamma=0.99,
         learning_rate=3e-4,
         eps_start=0.9,
         eps_end=0.05,
         eps_decay=1000,
+        update_method="hard",
         target_update=200,
     )
     .network(
         conv_layers=(), hidden_units=(128, 128), network_type=NetworkType.FEED_FORWARD
     )
     .environment(env=env)
-    .training()
+    .training(lr_scheduler="cyclic")
     # .training("model_1500:v0")
     .debugging(log_level="INFO")
     .rendering()
