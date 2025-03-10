0e3e876d041f3f55ab1363b11c7c8a3fdbf6a07d
eirik <eirik@reiestad.no>
2025-03-10 15:37:00 +0100

    feat: fix pipeline

--- xailib/common/completeness_score.py
+++ xailib/common/completeness_score.py
@@ -206,8 +206,6 @@ def get_completeness_score_network(
     if "random" in concepts:
         concepts.remove("random")
 
-    path = os.path.join(result_path, f"concept_combination_accuracies_{layer_idx}.json")
-    write_results(results, path)
     if len(concepts) > 8:
         logging.warning(
             "The number of concepts is greater than 8. This will take a long time to compute, soooo we will not do it:)."
@@ -243,5 +241,7 @@ def get_completeness_score_network(
         avg_sub_accuracy /= epochs
         results[tuple(sorted(comb))] = (avg_sub_loss, avg_sub_accuracy)
 
+    path = os.path.join(result_path, f"concept_combination_accuracies_{layer_idx}.json")
+    write_results(results, path)
     shapley_values = calculate_shapley_values(path, concepts)
     write_results(shapley_values, os.path.join(result_path, filename_shapley))
