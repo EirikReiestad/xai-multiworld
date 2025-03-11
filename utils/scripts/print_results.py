da7febb33fc725453c1adaa37abf20781aaf3823
eirik <eirik@reiestad.no>
2025-03-11 16:18:06 +0100

    feat: json data to latex format

--- utils/scripts/print_results.py
+++ utils/scripts/print_results.py
@@ -6,7 +6,6 @@ from typing import Dict, Tuple
 
 import pandas as pd
 from joblib.pool import np
-from pandas.io.formats.style import Styler
 
 from utils.core.plotting import plot_3d
 
@@ -50,7 +49,7 @@ def main(path: str = os.path.join("assets", "results")):
                 data = json.load(f)
                 df, latex = tabulate_data(data, filename)
                 if df is None:
-                    break
+                    continue
                 latex_filename = filename.replace(".json", ".tex")
                 latex_filepath = os.path.join(path, latex_filename)
                 with open(latex_filepath, "w") as f:
@@ -81,14 +80,24 @@ def tabulate_data(data: Dict, filename: str):
 
     files = {
         "cav_similarity.json": tabulate_similarity_matrix,
-        "concept_combination_accuracies.json": tabulate_concept_combination_accuracies,
+        "concept_cav_similarity.json": tabulate_similarity_matrix,
+        "cos_sim_matrix.json": tabulate_similarity_matrix,
+        "cav_statistics.json": tabulate_cav_statistics,
+        "probe_statistics.json": tabulate_cav_statistics,
+        "concept_cav_statistics.json": tabulate_cav_statistics,
+        "cav_training_stats.json": tabulate_training_stats,
+        "concept_combination_accuracies.json": tabulate_combination_accuracies,
         "probe_robustness.json": tabulate_robustness,
-        "probe_statistics.json": tabulate_probe_statistics,
-        "cav_statistics.json": tabulate_probe_statistics,
         "sample_efficiency.json": tabulate_sample_efficiency,
+        "concept_score_decision_tree.json": tabulate_concept_score_decision_tree,
+        "shapley_values.json": tabulate_shapley_values,
     }
 
-    df, latex = files[filename](data)
+    try:
+        df, latex = files[filename](data)
+    except KeyError:
+        logging.error(f"File {filename} not found in tabulate_data")
+        return None, None
 
     logging.info("\n\n" + filename)
     logging.info(f"\n{df}")
@@ -96,11 +105,96 @@ def tabulate_data(data: Dict, filename: str):
     return df, latex
 
 
-def tabulate_probe_statistics(data: Dict) -> Tuple[pd.DataFrame, str]:
+def tabulate_generic(data: Dict) -> Tuple[pd.DataFrame, str]:
     df = pd.DataFrame(data)
-    df.columns = df.columns.str.replace("_", r"\_")
-    df.index = df.index.str.replace("_", r"\_")
-    styled_df = df.style
+    latex = df.to_latex()
+    return df, latex
+
+
+def tabulate_shapley_values(data: Dict) -> Tuple[pd.DataFrame, str]:
+    df = pd.DataFrame([data])
+    df = df.transpose()
+    df.columns = ["Shapley Value"]
+    df.sort_values(by="Shapley Value", ascending=False, inplace=True)
+    styled_df = df.copy()
+    styled_df = styled_df.style
+    latex = styled_df.to_latex()
+    latex = format_table(latex)
+    return df, latex
+
+
+def tabulate_robustness(data: Dict) -> Tuple[pd.DataFrame, str]:
+    processed_data = {}
+    for condition, values in data.items():
+        if condition not in processed_data:
+            processed_data[condition] = {}
+        for key, value in values.items():
+            processed_data[condition][key] = value[0][0]
+    data = processed_data
+    df = pd.DataFrame(data)
+    styled_df = df.copy()
+    styled_df.index = styled_df.index.str.replace("_", r"\_")
+    styled_df.columns = styled_df.columns.str.replace("_", r"\_")
+    styled_df = styled_df.style
+    styled_df.apply(
+        lambda s: highlight_threshold(s, props="bfseries:;", min=0.7, max=1.0),
+        axis=0,
+    )
+    latex = styled_df.to_latex()
+    latex = format_table(latex)
+    return df, latex
+
+
+def tabulate_concept_score_decision_tree(data: Dict) -> Tuple[pd.DataFrame, str]:
+    df = pd.DataFrame(data)
+    df = df.transpose()
+    df.columns = ["Importance", "Splits"]
+    df.sort_values(by="Importance", ascending=False, inplace=True)
+    styled_df = df.copy()
+    styled_df.index = styled_df.index.str.replace("_", r"\_")
+    styled_df = styled_df.style
+    latex = styled_df.to_latex()
+    latex = format_table(latex)
+    return df, latex
+
+
+def tabulate_combination_accuracies(data: Dict) -> Tuple[pd.DataFrame, str]:
+    df = pd.DataFrame(data)
+    df = df.transpose()
+    df.columns = ["Loss", "Accuracy"]
+    df.sort_values(by="Loss", ascending=True, inplace=True)
+    styled_df = df.copy()
+    styled_df.index = styled_df.index.str.replace("_", r"\_")
+    styled_df = styled_df.style
+    styled_df.apply(
+        lambda s: highlight_max(s, props="color:{f_green}; bfseries:;"),
+        axis=0,
+    )
+    styled_df.apply(
+        lambda s: highlight_min(s, props="color:{f_darkred}; bfseries:;"),
+        axis=0,
+    )
+    latex = styled_df.to_latex()
+    latex = format_table(latex)
+    return df, latex
+
+
+def tabulate_training_stats(data: Dict) -> Tuple[pd.DataFrame, str]:
+    df = pd.DataFrame(data, index=None)
+    styled_df = df.copy()
+    styled_df.columns = styled_df.columns.str.replace("_", r"\_")
+    styled_df.index = styled_df.index.str.replace("_", r"\_")
+    latex = styled_df.to_latex(index=False)
+    latex = format_table(latex)
+    return df, latex
+
+
+def tabulate_cav_statistics(data: Dict) -> Tuple[pd.DataFrame, str]:
+    df = pd.DataFrame(data)
+    styled_df = df.copy()
+    styled_df.columns = styled_df.columns.str.replace("_", r"\_")
+    styled_df.index = styled_df.index.str.replace("_", r"\_")
+    styled_df = styled_df.style
     styled_df.apply(
         lambda s: highlight_max(s, props="color:{f_green}; bfseries:;"),
         axis=1,
@@ -116,13 +210,22 @@ def tabulate_probe_statistics(data: Dict) -> Tuple[pd.DataFrame, str]:
 
 def tabulate_similarity_matrix(data: Dict) -> Tuple[pd.DataFrame, str]:
     df = pd.DataFrame(data)
-    styled_df = df.style
+    np_df = df.to_numpy()
+    tril = np.tril(np_df)
+    mask = np.triu(np.ones(tril.shape), k=1).astype(bool)
+    tril[mask] = np.nan
+    df = pd.DataFrame(tril, columns=df.columns, index=df.index)
+    styled_df = df.copy()
+    styled_df.columns = styled_df.columns.str.replace("_", r"\_")
+    styled_df.index = styled_df.index.str.replace("_", r"\_")
+    styled_df = styled_df.style
     styled_df.apply(
         lambda s: highlight_between_threshold(
             s, props="color:{f_green}; bfseries:;", min=0.7, max=1.0
         ),
         axis=1,
     )
+    styled_df.apply(highlight_nan_values, props="color:{f_white}; bfseries:;", axis=1)
     latex = styled_df.to_latex()
     latex = format_table(latex)
     return df, latex
@@ -130,21 +233,6 @@ def tabulate_similarity_matrix(data: Dict) -> Tuple[pd.DataFrame, str]:
 
 def tabulate_sample_efficiency(data: Dict) -> Tuple[pd.DataFrame, str]:
     pass
-
-
-def tabulate_concept_combination_accuracies(data: Dict) -> Tuple[pd.DataFrame, str]:
-    pass
-
-
-def tabulate_robustness(data: Dict) -> Tuple[pd.DataFrame, str]:
-    df = pd.DataFrame(
-        [
-            {"Condition": condition, "Key": key, "Value": value[0][0]}
-            for condition, values in data.items()
-            for key, value in values.items()
-        ]
-    )
-    return df.pivot(index="Key", columns="Condition", values="Value").iloc[::-1]
 
 
 def tabulate_combination_accuracy(data: Dict):
@@ -178,6 +266,10 @@ def highlight_between_threshold(
 ):
     mask = (s > min) & (s < max)
     return [props if v else "" for v in mask]
+
+
+def highlight_nan_values(s, props: str):
+    return np.where(pd.isna(s), props, "")
 
 
 if __name__ == "__main__":
