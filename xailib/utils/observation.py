a31746c445e5f3a2462c816661903f6f6f331f96
eirik <eirik@reiestad.no>
2025-05-01 11:21:01 +0200

    fix: added filter observation, that hopefully will filter observations:)

--- xailib/utils/observation.py
+++ xailib/utils/observation.py
@@ -1,6 +1,10 @@
 from typing import Dict, List, Tuple
 
-from utils.common.observation import Observation, load_and_split_observation
+from utils.common.observation import (
+    Observation,
+    filter_observations,
+    load_and_split_observation,
+)
 
 
 def get_observations(
@@ -17,15 +21,22 @@ def get_observations(
     test_negative_observations = {}
 
     for concept in concepts:
-        positive_observation, test_observation = load_and_split_observation(
+        positive_observation, test_positive_observation = load_and_split_observation(
             concept, 0.8
         )
-        negative_observation, _ = load_and_split_observation("negative_" + concept, 0.8)
+        negative_observation, test_negative_observation = load_and_split_observation(
+            "negative_" + concept, 0.8
+        )
+
+        positive_observation = filter_observations(positive_observation)
+        negative_observation = filter_observations(negative_observation)
+        test_positive_observation = filter_observations(test_positive_observation)
+        test_negative_observation = filter_observations(test_negative_observation)
 
         positive_observations[concept] = positive_observation
         negative_observations[concept] = negative_observation
-        test_positive_observations[concept] = test_observation
-        test_negative_observations[concept] = negative_observation
+        test_positive_observations[concept] = test_positive_observation
+        test_negative_observations[concept] = test_negative_observation
 
     return (
         positive_observations,
